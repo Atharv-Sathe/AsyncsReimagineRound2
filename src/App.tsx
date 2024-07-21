d48aeb521f729b616064638b4ed8758a8d9b5ac9
Ayush Pandey <ayush12122003@gmail.com>
2024-07-21 10:10:27 +0530

    feat: Add carousel component for image slideshow

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Section2 from "./components/section2";
 import Section3 from "./components/section3";
 import Showcase from "./components/showcase";
 import { useEffect, useState } from "react";
+import Carousel from "./components/carouseluse";
+import Slider from "./components/videoCarousel";
 
 function App() {
   const [isLoading, SetIsLoading] = useState(true);
@@ -27,6 +29,7 @@ function App() {
           <Section2 />
           <Showcase />
           <Section3 />
+          <Carousel/>
         </>
       )}
     </>
---
+++ src/components/videoCarousel.tsx
@@ -0,0 +1,77 @@
+import { useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { cn } from "../cn";
+
+export default function Slider({ className }: { className?: string }) {
+  const progressCircle = useRef<SVGSVGElement>(null);
+  const progressContent = useRef<HTMLSpanElement>(null);
+
+  const onAutoplayTimeLeft = (s: unknown, time: number, progress: number) => {
+    if (progressCircle.current) {
+      progressCircle.current.style.setProperty("--progress", `${1 - progress}`);
+    }
+    if (progressContent.current) {
+      progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  };
+
+  return (
+    <Swiper
+      spaceBetween={30}
+      centeredSlides={true}
+      autoplay={{
+        delay: 10000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+      onAutoplayTimeLeft={onAutoplayTimeLeft}
+      className={cn("Swiper rounded-lg", className)}
+    >
+      <SwiperSlide className="swiper-slide flex justify-center items-center">
+        <video className="w-full h-full object-cover " muted loop>
+          <source src="/bluestone.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </SwiperSlide>
+      <SwiperSlide className="swiper-slide flex justify-center items-center">
+        <video className="w-full h-full object-cover " muted loop>
+          <source src="/bluestone.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </SwiperSlide>
+      <SwiperSlide className="swiper-slide flex justify-center items-center">
+        <video className="w-full h-full object-cover " muted loop>
+          <source src="/bluestone.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </SwiperSlide>
+      <SwiperSlide className="swiper-slide flex justify-center items-center">
+        <video className="w-full h-full object-cover " muted loop>
+          <source src="/bluestone.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </SwiperSlide>
+      <SwiperSlide className="swiper-slide flex justify-center items-center">
+        <video className="w-full h-full object-cover " muted loop>
+          <source src="/bluestone.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </SwiperSlide>
+      <div className="autoplay-progress" slot="container-end">
+        <svg viewBox="0 0 48 48" ref={progressCircle}>
+          <circle cx="24" cy="24" r="20"></circle>
+        </svg>
+        <span ref={progressContent}></span>
+      </div>
+    </Swiper>
+  );
+}
