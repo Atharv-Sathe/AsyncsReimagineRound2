5840ecd1e9bf4a9ec29b12dbe4ee1bb90ed25047
Ayush Pandey <ayush12122003@gmail.com>
2024-07-21 23:30:19 +0530

    feat: Add Section4 component for video display and text overlay

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import Preloader from "./components/preloader";
 // import Section1 from "./components/section1";
 import Section2 from "./components/section2";
 // import Section3 from "./components/section3";
-import Showcase from "./components/showcase";
+// import Showcase from "./components/showcase";
 import { useEffect, useState } from "react";
 import ReactLenis from "lenis/react";
 import Slider from "./components/videoCarousel";
+import Section4 from "./components/section4";
+// import ImageCarousel from "./components/section3";
+import Showcase1 from "./components/showcase1";
+import Showcase from "./components/showcase";
+import Section5 from "./components/section5";
 
 function App() {
   const [isLoading, SetIsLoading] = useState(true);
@@ -29,7 +34,9 @@ function App() {
           <Slider />
           <Section2 />
           <Showcase />
-          {/* <Section3 /> */}
+          {/* <ImageCarousel /> */}
+          <Section4 />
+          <Section5 />
         </ReactLenis>
       )}
     </>
