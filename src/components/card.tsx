e516167afa42bb649786b4410206df5305573d3e
Ayush Pandey <ayush12122003@gmail.com>
2024-07-25 20:29:51 +0530

    Ayush commit

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,8 +1,30 @@
+import { cn } from "../cn";
 
-export default function card() {
+type CardProps = {
+  title: string;
+  description: string;
+  backgroundImage: string;
+};
+
+export default function Card({ title, description, backgroundImage }: CardProps) {
   return (
-    <div className="flex fle">
-      
+    <div className="max-w-xs w-full">
+      <div
+        className={cn(
+          "group w-full cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl mx-auto flex flex-col justify-end p-2 border border-transparent ",
+          "bg-cover"
+        )}
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
+        <div className="text relative z-50">
+          <h1 className="font-bold text-xl md:text-3xl text-gray-500 relative mix-blend-overlay">
+            {title}
+          </h1>
+          <p className="font-normal text-base text-gray-500 relative my-4">
+            {description}
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
